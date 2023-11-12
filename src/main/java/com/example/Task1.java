b59b613e768d626d509c95dd9f83efd0fdbef04e
DN070578NOI <oleksandr.nikolaichuk@privatbank.ua>
2023-11-12 11:08:13 +0100

    Task1.java

--- src/main/java/com/example/Task1.java
+++ src/main/java/com/example/Task1.java
@@ -4,4 +4,49 @@ package com.example;
  * Created by dn070578noi on 12.11.23.
  */
 public class Task1 {
+
+    public int getMinimumSwapCounts(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int onesCount = 0;
+        // find size of the window
+        for (int k : array) {
+            if (k == 1) {
+                onesCount += 1;
+            }
+        }
+        int windowSize = onesCount;
+
+        // look in the first window
+        int windowOnesCount = 0;
+        for (int i = 0; i < windowSize; i++) {
+            if (array[i] == 1) {
+                windowOnesCount += 1;
+            }
+        }
+        // max = count in the first window
+        int maxOnesCount = windowOnesCount;
+
+        int windowStart = 1;
+        int windowEnd = windowSize;
+        // move window to the right
+        while (windowEnd < array.length) {
+            // if left element is 1 then decrease count
+            if (array[windowStart - 1] == 1) {
+                windowOnesCount -= 1;
+            }
+            // if right element is 1 then increase count
+            if (array[windowEnd] == 1) {
+                windowOnesCount += 1;
+            }
+            // update max
+            maxOnesCount = Math.max(maxOnesCount, windowOnesCount);
+            // move window to the right
+            windowEnd += 1;
+            windowStart += 1;
+        }
+        // return size of the window - max count of all windows
+        return windowSize - maxOnesCount;
+    }
 }
--- src/test/java/com/example/Task1Test.java
+++ src/test/java/com/example/Task1Test.java
@@ -1,7 +1,62 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Task1Test {
+    private final Task1 task1 = new Task1();
+    @Test
+    void findMinimumSwapsWhenOneSwapNeed() {
+        int[] input = {1, 0, 0, 1, 1};
+        assertEquals(1, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findMinimumSwapsWhenSomeSwapsNeed() {
+        int[] input = {1, 0, 0, 1, 1, 0, 1, 0, 1};
+        assertEquals(2, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findZeroIfNoOnes() {
+        int[] input = {0, 0, 0, 0, 0};
+        assertEquals(0, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findZeroIfNoZeros() {
+        int[] input = {1, 1, 1, 1, 1};
+        assertEquals(0, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findZeroIfEmptyArray() {
+        int[] input = {};
+        assertEquals(0, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findZeroIfNull() {
+        assertEquals(0, task1.getMinimumSwapCounts(null));
+    }
+
+    @Test
+    void findZeroIfOneElement() {
+        int[] input = {1};
+        assertEquals(0, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void findZeroIfTwoElements() {
+        int[] input = {1, 1};
+        assertEquals(0, task1.getMinimumSwapCounts(input));
+    }
+
+    @Test
+    void testForLongArray() {
+        int[] input = {1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1};
+        assertEquals(4, task1.getMinimumSwapCounts(input));
+    }
 
 }

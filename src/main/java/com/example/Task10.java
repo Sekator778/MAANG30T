161182ad3c61b1adffd90b8e807833a15cdb9bac
DN070578NOI <oleksandr.nikolaichuk@privatbank.ua>
2023-12-09 22:32:38 +0100

    Task10.java

--- src/main/java/com/example/Task10.java
+++ src/main/java/com/example/Task10.java
@@ -3,7 +3,7 @@ package com.example;
 import com.example.utility.ListNode;
 
 /**
- * Created by dn070578noi on 29.11.23.
+ * Created by dn070578noi on 09.12.23.
  */
 public class Task10 {
     public ListNode partition(ListNode head, int x) {

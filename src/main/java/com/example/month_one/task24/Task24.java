943b78c83c3174f9fa5241b95f842e9479c5dabc
DN070578NOI <oleksandr.nikolaichuk@privatbank.ua>
2024-01-06 17:53:01 +0100

    Опис 24

--- src/main/java/com/example/month_one/task24/Task24.java
+++ src/main/java/com/example/month_one/task24/Task24.java
@@ -7,11 +7,15 @@ import java.util.Stack;
  */
 public class Task24 {
     public int[] dailyTemperatures(int[] temperatures) {
+        // створюємо стек, в який будемо додавати індекси елементів масиву (дні)
         Stack<Integer> decStack = new Stack<>();
         int[] temperatureDistance = new int[temperatures.length];
 
         for (int end = 0; end < temperatures.length; end++) {
             while (!decStack.isEmpty()) {
+                // якщо температура в день, який є в стеку, більша за температуру в поточному дні,
+                // то видаляємо індекс дня зі стеку
+                // якщо ні - виходимо з циклу
                 int start = decStack.peek();
                 if (temperatures[start] >= temperatures[end]) break;
 

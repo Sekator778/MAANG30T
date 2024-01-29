458b9f92e42e5bb3a029b793b43013b47aeb6bbb
Aleksandr Nikolaichuk <sekator778@gmail.com>
2024-01-29 07:50:20 +0100

    small task is word palindrome?

---
+++ src/test/java/com/example/month_two/Task4.java
@@ -0,0 +1,21 @@
+package com.example.month_two;
+
+public class Task4 {
+    /**
+     * is word palindrome?
+     */
+    public boolean isWordPalindrome(String word) {
+        if (word.length() <= 1) return true;
+
+        int left = 0;
+        int right = word.length() - 1;
+
+        return isWordPalindrome(left, right, word);
+    }
+
+    private boolean isWordPalindrome(int left , int right, String word) {
+        if (left >= right) return true;
+        if (word.charAt(left) != word.charAt(right)) return false;
+        return isWordPalindrome(left + 1, right - 1, word);
+    }
+}
---
+++ src/test/java/com/example/month_two/Task4Test.java
@@ -0,0 +1,28 @@
+package com.example.month_two;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task4Test {
+
+    @Test
+    void isWordPalindrome() {
+        assertTrue(new Task4().isWordPalindrome("aba"));
+    }
+
+    @Test
+    void isWordPalindrome2() {
+        assertFalse(new Task4().isWordPalindrome("abac"));
+    }
+
+    @Test
+    void isWordPalindrome3() {
+        assertTrue(new Task4().isWordPalindrome("aaaaaaaaaa"));
+    }
+
+    @Test
+    void isWordPalindrome4() {
+        assertTrue(new Task4().isWordPalindrome("dcbabcd"));
+    }
+}

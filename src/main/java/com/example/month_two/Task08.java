13e246a31ea3b4b49f18c3bfd00fd71bde5bd0c6
Aleksandr Nikolaichuk <sekator778@gmail.com>
2024-02-02 09:01:37 +0100

    task08

---
+++ src/main/java/com/example/month_two/Task08.java
@@ -0,0 +1,22 @@
+package com.example.month_two;
+
+public class Task08 {
+    public int findTargetSumWays(int[] nums, int target) {
+      return  helper(target, 0, nums);
+    }
+
+    private int helper(int target, int index, int[] num) {
+        if (index == num.length) {
+            if (target == 0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+
+     return   helper(target - num[index], index+1, num) +
+        helper(target + num[index], index+1, num);
+    }
+
+}
---
+++ src/test/java/com/example/month_two/Task08Test.java
@@ -0,0 +1,37 @@
+package com.example.month_two;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task08Test {
+//    Example 1:
+//
+//    Input: nums = [1,1,1,1,1], target = 3
+//    Output: 5
+//    Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+//            -1 + 1 + 1 + 1 + 1 = 3
+//            +1 - 1 + 1 + 1 + 1 = 3
+//            +1 + 1 - 1 + 1 + 1 = 3
+//            +1 + 1 + 1 - 1 + 1 = 3
+//            +1 + 1 + 1 + 1 - 1 = 3
+    @Test
+    void findTargetSumWays() {
+        Task08 task08 = new Task08();
+        int[] nums = {1, 1, 1, 1, 1};
+        int target = 3;
+        assertEquals(5, task08.findTargetSumWays(nums, target));
+    }
+
+//    Example 2:
+//
+//    Input: nums = [1], target = 1
+//    Output: 1
+    @Test
+    void findTargetSumWays2() {
+        Task08 task08 = new Task08();
+        int[] nums = {1};
+        int target = 1;
+        assertEquals(1, task08.findTargetSumWays(nums, target));
+    }
+}

6c26ff7603abaa60dda40bc2d3b0388db99b8965
Ніколайчук Олександр Євгенійович <oleksandr.nikolaichuk@privatbank.ua>
2024-02-16 12:35:07 +0100

    add docs 1718

--- src/main/java/com/example/recursion/Task1718.java
+++ src/main/java/com/example/recursion/Task1718.java
@@ -11,31 +11,39 @@ public class Task1718 {
         if (seqStartIndex == seqCandidate.length) {
             return true;
         }
+        // якщо seqCandidate[seqStartIndex] > 0, то вже є число на цьому місці
         if (seqCandidate[seqStartIndex] > 0) {
+            // тому переходимо до наступного елементу
             return getSequence(seqCandidate, seqStartIndex + 1, usedNums, lasNum);
         }
-
+        // початок з найбільшого числа
         for (int num = lasNum; num >= 1; num--) {
             if (usedNums[num]) {
                 continue;
             }
+            // якщо num == 1, то використовуємо його одразу
             if (num == 1) {
                 seqCandidate[seqStartIndex] = 1;
             } else {
+                // якщо num > 1, то перевіряємо чи можна використати його у відповідних індексах
                 if (seqStartIndex + num >= seqCandidate.length || seqCandidate[seqStartIndex + num] > 0) {
                     continue;
                 }
                 seqCandidate[seqStartIndex] = num;
                 seqCandidate[seqStartIndex + num] = num;
             }
+            // помічаємо, що num вже використано
             usedNums[num] = true;
-
+            // рекурсивно шукаємо наступний елемент
             if (getSequence(seqCandidate, seqStartIndex + 1, usedNums, lasNum)) {
                 return true;
             }
+            // якщо не знайшли, то звільняємо num
             usedNums[num] = false;
+            // якщо num == 1, то звільняємо його одразу
             seqCandidate[seqStartIndex] = 0;
             if (num > 1) {
+                // якщо num > 1, то звільняємо його у відповідних індексах
                 seqCandidate[seqStartIndex + num] = 0;
             }
         }

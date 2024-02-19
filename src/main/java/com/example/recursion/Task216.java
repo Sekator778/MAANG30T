8340a180b198411b59e94171d7556d4fb6e73a7d
Oleksandr Nikolaichuk <sekator778@gmail.com>
2024-02-19 11:49:18 +0100

    216

--- src/main/java/com/example/recursion/Task216.java
+++ src/main/java/com/example/recursion/Task216.java
@@ -11,17 +11,16 @@ public class Task216 {
     }
 
     private void getSumsWays(List<Integer> candidate, int sum, int startNum, int termsCount, List<List<Integer>> sumWays) {
-        if (sum < 0) {
-            return;
-        }
         if (termsCount == 0) {
             if (sum == 0) {
                 sumWays.add(new ArrayList<>(candidate));
             }
             return;
         }
-
         for (int num = startNum; num <= 9; num++) {
+            if (sum < 0) {
+                return;
+            }
             candidate.add(num);
             getSumsWays(candidate, sum - num, num + 1, termsCount - 1, sumWays);
             candidate.removeLast();
